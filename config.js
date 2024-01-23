7d758fc43069b2df47b9d9e88a4b64d841e43279
zackblacchope <137235317+zackblacchope@users.noreply.github.com>
2024-01-23 23:21:29 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['33757054415', 'Abraham Dwamena3', true],
+  [254750575934', 'Zack', true]
   
 ] //Number of owners
 
